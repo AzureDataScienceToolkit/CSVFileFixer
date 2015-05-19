fabf07e9d397aa6b53be4df4baaae639773533d4
Michal Zylinski <michal.zylinski@gmail.com>
2015-05-19 17:06:39 +0200

    Added replace, delimiter and quote parameters

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# CSVFileFixer
+# EOLFixer
+Simple Python script that either removes or replaces EOL characters that may appear in CSV files.
+
+##Script syntax
+<code>
+eolfixer.py [-h] [--replace REPLACE] [--delimiter DELIMITER] 
+ [--quote QUOTE] [-v] input output                                     
+                                                                    
+Removes or replaces unnecesary EOL characters in CSV files.         
+                                                                    
+    positional arguments:                                               
+        input                   name of input file                          
+        output                  name of output file                         
+                                                                    
+    optional arguments:                                                 
+         -h, --help             show this help message and exit             
+        --replace REPLACE       character to replace EOL with (default ' ') 
+        --delimiter DELIMITER   delimiter (default ',')                     
+        --quote QUOTE           quote character (default '"')               
+         -v                     verbose-prints some useful information      
+</code>
+
+##Basic usage examples
+
+###Replacing EOLs with spaces in simple CSV files
+<code>
+eolfixer.py inputfile.csv outputfile.csv
+</code>
+###Replacing EOLs with spaces in TSV files
+<code>
+eolfixer.py inputfile.csv outputfile.csv --delimiter '\t'
+</code>
--- eolfixer.py
+++ eolfixer.py
@@ -1,23 +1,31 @@
 import csv, argparse
 
-parser = argparse.ArgumentParser(description="Replaces unnecesary EOL characters in CSV files.")
+parser = argparse.ArgumentParser(description="Removes or replaces unnecesary EOL characters in CSV files.")
 parser.add_argument('input', type=str, help='name of input file')
 parser.add_argument('output', type=str, help='name of output file')
+parser.add_argument('--replace', type=str, default=" ", help="character to replace EOL with (default \' \')")
+parser.add_argument('--delimiter', type=str, default=",", help='delimiter (default \',\')')
+parser.add_argument('--quote', type=str, default='"', help='quote character (default \'"\')')
 parser.add_argument('-v', help="verbose-prints some useful information", action="store_true")
 args = parser.parse_args()
 
 error_counter = 0
 
 with open(args.input) as csvfile:
-    linereader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    linewriter = csv.writer(file(args.output, "wb"))
+    if args.delimiter == "\\t": args.delimiter = "\t"
+    linereader = csv.reader(csvfile, delimiter=args.delimiter, quotechar=args.quote)
+    if args.output:
+        linewriter = csv.writer(file(args.output, "wb"), delimiter=args.delimiter, quotechar=args.quote)
+    else:
+        linewriter = None
 
     for line in linereader:
         for field_no in range(len(line)):
             if line[field_no].find("\n") > -1:
-                line[field_no] = line[field_no].replace("\n", " ")
+                line[field_no] = line[field_no].replace("\n", args.replace)
                 error_counter += 1
         linewriter.writerow(line)
+
 if args.v:
-    print "Number of characters replaced: " + error_counter
+    print "Job done. Number of characters fixed: " + str(error_counter)
 
